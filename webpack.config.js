7c2a80c15f071bbdbf4b78ebe1112005ff71fdba
usermf <usermf@localhost.localdomain>
2017-10-16 08:44:03 +0300

    webpack sass config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,43 @@
-var path = require("path");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin("style.css");
 module.exports = {
   entry: {
-    app: ["./src/app.ts"]
+    app: "./src/app.ts",
+    style: "./sass/style.sass"
   },
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   resolve: {
     // Add '.ts' and '.tsx' as a resolvable extension.
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", "sass"]
   },
   module: {
     loaders: [
       // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.sass$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      }
     ]
   },
   devServer: {
     contentBase: "./build",
     port: 8080
-  }
+  },
+  plugins: [extractSass]
 };
